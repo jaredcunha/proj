532e8b9288976a5744df22517c828bbb305e630f
Lance Ito <70550863+Pete803@users.noreply.github.com>
2021-03-23 20:45:51 -0400

    update

---
+++ ppp_df.sql
@@ -0,0 +1,13 @@
+CREATE TABLE ppp_data (
+	loan_amount DECIMAL,
+	city VARCHAR(30),
+	zip INTEGER,
+	naics_code VARCHAR(30),
+	business_type VARCHAR(30),
+	lender VARCHAR(30),
+	cd VARCHAR(30)
+);
+
+SELECT * from ppp_df
+
+
